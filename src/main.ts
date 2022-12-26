ae6b800c267a8039a02b42131f25afb925e1e933
amenpunk <ondasycircuitos@gmail.com>
2022-12-26 13:03:22 -0600

    keydown observer

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import { fromEvent } from 'rxjs';
 
-// document.addEventListener('DOMContentLoaded',() => {
-//   console.log('document ready')
-// })
-//
-const onMouseMove$ = fromEvent(document, "mousemove")
+const onKeyDown$ = fromEvent<KeyboardEvent>(document, "keydown")
 
-const observerMouse = {
-  next: (event: MouseEvent) => {
-    console.log(event)
+let letterIndex: number = 0;
+let letterRowIndex: number = 0;
+
+let letterRows = document.getElementsByClassName('letter-row')
+
+const insertLetter = {
+  next: (event: KeyboardEvent) => {
+    let key = event.key;
+    if(key.length === 1 && key.match(/[a-z]/i)){
+      console.log(key);
+      console.log(letterRows)
+      let letterBox :HTMLDivElement  = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+      letterBox.textContent = key;
+      letterBox.classList.add("filled-letter");
+      letterIndex++;
+    }
   },
   complete: () => console.log('complete'),
   error: (e: Error) => console.error('error', e.message),
 }
 
-onMouseMove$.subscribe(observerMouse)
+onKeyDown$.subscribe(insertLetter)
