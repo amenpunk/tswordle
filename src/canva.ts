cabb6c2124f8632b866b0c95ef74ccac237039e3
amenpunk <ondasycircuitos@gmail.com>
2023-01-05 11:41:59 -0600

    draw

--- src/canva.ts
+++ src/canva.ts
@@ -1,26 +1,45 @@
-import { fromEvent, map } from "rxjs"
+import { fromEvent } from "rxjs"
+import { takeUntil, map, mergeAll } from 'rxjs/operators'
 
-const canvas : HTMLCanvasElement = document.getElementById('reactive-canvas')
+const canvas: HTMLCanvasElement = document.getElementById('reactive-canvas')
 var cursorPosition = { x: 0, y: 0 };
 
+const updateCursorPosition = (event: MouseEvent) => {
+  cursorPosition.x = event?.clientX - canvas?.offsetLeft;
+  cursorPosition.y = event?.clientY - canvas?.offsetTop;
+  console.log(cursorPosition)
+}
+
 const onMouseDown$ = fromEvent(canvas, "mousedown").pipe(
-  map( event  => {
-    cursorPosition.x =  event?.clientX - canvas?.offsetLeft;
-    cursorPosition.y =  event?.clientY - canvas?.offsetTop;
-    console.log(cursorPosition)
+  map(event => {
+    updateCursorPosition(event)
   })
 )
-// const onMouseUp$ = fromEvent(canvas, "mouseup")
-// const onMouseMove$ = fromEvent(canvas, "mousemove")
+const onMouseUp$ = fromEvent(canvas, "mouseup")
+const onMouseMove$ = fromEvent(canvas, "mousemove").pipe(takeUntil(onMouseUp$))
 
 onMouseDown$.subscribe();
 
 const canvasContext = canvas.getContext("2d");
 canvasContext.lineWidth = 8;
+canvasContext.lineJoin = "round";
+canvasContext.lineCap = "round";
 canvasContext.strokeStyle = "white";
 
-canvasContext.beginPath()
-canvasContext.moveTo(0, 0)
-canvasContext.lineTo(100, 100)
-canvasContext.stroke()
-canvasContext.closePath()
+
+function paintStroke(event: MouseEvent): void {
+  canvasContext?.beginPath()
+  canvasContext?.moveTo(cursorPosition.x, cursorPosition.y)
+  updateCursorPosition(event)
+  canvasContext?.lineTo(cursorPosition.x, cursorPosition.y)
+  canvasContext?.stroke()
+  canvasContext?.closePath()
+}
+
+
+const startPaint$ = onMouseDown$.pipe(
+  map(() => onMouseMove$),
+  mergeAll()
+)
+
+startPaint$.subscribe(paintStroke)
---
+++ src/until.ts
@@ -0,0 +1,9 @@
+import { fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators'
+
+const onMouseMove$ = fromEvent(document, 'mousemove')
+const onMouseDown$ = fromEvent(document, 'mousedown')
+
+const sourceCompleted = onMouseMove$.pipe(takeUntil(onMouseDown$))
+
+sourceCompleted.subscribe(console.log)
